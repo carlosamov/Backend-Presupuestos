8a0ebe2ce55f069d078b614979df66bcfa6ee233
Carlos Ovalles <carlosamov@gmail.com>
2026-04-20 13:05:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -50,6 +50,7 @@ const corsOptions = {
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
+  optionsSuccessStatus: 200
 };
 
 app.use(cors(corsOptions));
